a19fae11d55fc503856dcf4174190d35b6f0b2e6
Angelica Perez <aperez9423@gmail.com>
2020-06-05 21:39:34 -0400

    added three events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+const headingsList = document.querySelectorAll('h2');
+
+headingsList[0].addEventListener('mouseover',(event) => {
+    headingsList[0].style.color = 'red';
+});
+
+const images = document.querySelectorAll('img');
+
+images[0].addEventListener('click', (event) => {
+    images[0].style.transform = 'scale(1.5)';
+    images[0].style.transition = 'transform 1s'
+});
+
+images[0].addEventListener('dblclick', (event) => {
+    images[0].style.transform = 'scale(1)';
+    images[0].style.transition = 'transform 1s'
+});
+
+const body = querySelector('body');
+
+window.addEventListener('click', () => {
+    document.body.style = 'color: white; background-color = black';
+});
+
+
+
+
