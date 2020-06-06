66d9e9e6d005f49f123d6d5fdcb6343cb35189dc
Angelica Perez <aperez9423@gmail.com>
2020-06-06 18:27:06 -0400

    completed nine events for project

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Your code goes here
 const headingsList = document.querySelectorAll('h2');
 
-headingsList[0].addEventListener('mouseover',(event) => {
+headingsList[0].addEventListener('mouseenter',(event) => {
     headingsList[0].style.color = 'red';
+});
+
+headingsList[0].addEventListener('mouseleave',(event) => {
+    headingsList[0].style.color = 'black';
 });
 
 const images = document.querySelectorAll('img');
@@ -17,11 +21,49 @@ images[0].addEventListener('dblclick', (event) => {
     images[0].style.transition = 'transform 1s'
 });
 
-const body = querySelector('body');
-
-window.addEventListener('click', () => {
-    document.body.style = 'color: white; background-color = black';
+images[1].addEventListener('mousedown', (event) => {
+    images[1].style.border = 'thick solid hotpink';
 });
+
+
+images[2].addEventListener('mousedown', (event) => {
+    images[2].style.border = 'thick solid hotpink';
+});
+
+images[3].addEventListener('mousedown', (event) => {
+    images[3].style.border = 'thick solid hotpink';
+});
+
+images[1].addEventListener('mouseup', (event) => {
+    images[1].style.border = 'thick solid black';
+});
+
+images[2].addEventListener('mouseup', (event) => {
+    images[2].style.border = 'thick solid black';
+});
+
+images[3].addEventListener('mouseup', (event) => {
+    images[3].style.border = 'thick solid black';
+});
+
+const nav = document.querySelectorAll('a');
+
+document.addEventListener ('keydown', event => {
+    nav[2].style.visibility = 'hidden';
+});
+
+document.addEventListener ('keyup', event => {
+    nav[2].style.visibility = 'visible';
+});
+
+const heading = document.querySelector('h1');
+
+window.addEventListener('scroll', () => {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    heading.style.color = 'purple';
+});
+
 
 
 
